4bf9a663b1153f5db5d75f7652735c9e74b76d5f
Radu Farcas <radu.farcas@evozon.com>
2017-09-27 14:27:49 +0300

    finished js calculations and added service worker

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,5 +1,17 @@
 /*****************************************************************************
    *
+   * Register Service Worker
+   *
+   ****************************************************************************/
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+				   .register('./service-worker.js')
+				   .then(function() { console.log('[ServiceWorker] Registered'); });
+}
+
+/*****************************************************************************
+   *
    * Event listeners for UI elements
    *
    ****************************************************************************/
@@ -10,28 +22,55 @@ function setEventHandlers() {
 	document.getElementById('whatIsPercentOf').addEventListener('click', function() {
 		calcWhatIsPercentOf();
 	});
-	document.getElementById('isPercentOf').addEventListener('click', function() {
-		calcIsPercentOf();
+	document.getElementById('isWhatPercentOf').addEventListener('click', function() {
+		calcIsWhatPercentOf();
 	});
-	document.getElementById('isWhatPercent').addEventListener('click', function() {
-		calcIsWhatPercent();
+	document.getElementById('isPercentOfWhat').addEventListener('click', function() {
+		calcIsPercentOfWhat();
 	});
 }
 
+/*****************************************************************************
+   *
+   * Function that calculates what is x% of y (first form)
+   *
+   ****************************************************************************/
+
 function calcWhatIsPercentOf() {
-	var a = document.forms.whatIsPercentOf.a.value/100,
+	let a = document.forms.whatIsPercentOf.a.value/100,
 		b = a*document.forms.whatIsPercentOf.b.value,
 		result = document.getElementById('result-one');
+
+		if (!a || !b) return;
 
 	result.innerText = b;
 }
 
-function calcIsPercentOf() {
-	var a = document.forms.isPercentOf.c.value,
-		b = document.forms.isPercentOf.d.value,
+/*****************************************************************************
+   *
+   * Function that calculates x% of y = ? (second form)
+   *
+   ****************************************************************************/
+
+function calcIsWhatPercentOf() {
+	let a = document.forms.isWhatPercentOf.c.value,
+		b = document.forms.isWhatPercentOf.d.value,
 		c = a/b,
 		d = c*100,
 		result = document.getElementById('result-two');
 
+	if (!a || !b) return;
+
 	result.innerText = d;
+}
+
+function calcIsPercentOfWhat() {
+	let a = document.forms.isPercentOfWhat.e.value,
+		b = document.forms.isPercentOfWhat.f.value/100,
+		c = a/b,
+		result = document.getElementById('result-three');
+
+		if (!a || !b) return;
+
+	result.innerText = c;
 }
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+var staticCacheName = 'percentage-static-v1'; // !!change the cache version if you change anything in a file
+var filesToCache = [
+	'/',
+	'/index.html',
+	'/scripts/calculator.js',
+	'/styles/main.css'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] install');
+  e.waitUntil(
+      caches.open(staticCacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+            cacheNames.filter(function(cacheName) {
+              return cacheName.startsWith('percentage-');
+            }).map(function(cacheName) {
+              return caches.delete(cacheName);
+            })
+          );
+      })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
